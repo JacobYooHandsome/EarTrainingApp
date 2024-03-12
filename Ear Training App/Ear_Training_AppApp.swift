6563ff8e4b8a91f7cc365b0601cecf7163054860
jacobyoohandsome <tolearneverything123@gmail.com>
2024-03-11 21:06:28 -0400

    Initial Commit

---
+++ Ear Training App/Ear_Training_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ear_Training_AppApp.swift
+//  Ear Training App
+//
+//  Created by Jacob Yoo on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Ear_Training_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
